e622f16eb0524cdbd717afd38eeb164268ab866d
Thea Ross <theadora.ross@tyson.com>
2020-04-22 14:29:51 -0500

    Update test client to meet  sdk requirements
    The test client required various modifications to handle errors, submit
    http payloads, and satisfy payload builder type references.

--- client/main.go
+++ client/main.go
@@ -17,7 +17,10 @@ package main
 
 import (
 	"bytes"
+<<<<<<< HEAD
 	"flag"
+=======
+>>>>>>> 86c23d9... Update test client to meet  sdk requirements
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -35,7 +38,23 @@ import (
 const XO_FAMILY_NAME = "xo"
 const XO_FAMILY_PREFIX = "5b7349"
 
+<<<<<<< HEAD
 func createPayload(xoFamilyVersion, gameName, space string) (sabre.ISabrePayloadBuilder, error) {
+=======
+var XoContract = sabre.Contract{
+	Name:    XO_FAMILY_NAME,
+	Version: XO_FAMILY_VERSION,
+}
+
+var privateKey = "5dd1641e1e434387a9b870f47af2eb6a2150209b2712856aa1e7fb78e1426a58"
+var signer = signing.NewCryptoFactory(
+	signing.CreateContext("secp256k1"),
+).NewSigner(
+	signing.NewSecp256k1PrivateKey([]byte(privateKey)),
+)
+
+func createPayload(gameName, space string) (sabre.ISabrePayloadBuilder, error) {
+>>>>>>> 86c23d9... Update test client to meet  sdk requirements
 
 	gameAddress := addressing.CalculateDeploymentAdddress(XO_FAMILY_NAME,
 		gameName)
@@ -45,7 +64,11 @@ func createPayload(xoFamilyVersion, gameName, space string) (sabre.ISabrePayload
 	return sabre.NewSabrePayloadBuilder(
 		sabre.WithAction(sabre_pb2.SabrePayload_EXECUTE_CONTRACT),
 		sabre.WithContractName(XO_FAMILY_NAME),
+<<<<<<< HEAD
 		sabre.WithContractVersion(xoFamilyVersion),
+=======
+		sabre.WithContractVersion(XO_FAMILY_VERSION),
+>>>>>>> 86c23d9... Update test client to meet  sdk requirements
 		sabre.WithInputs([]string{gameAddress}),
 		sabre.WithOutputs([]string{gameAddress}),
 		sabre.WithExecuteContractPayload(payload),
@@ -53,8 +76,12 @@ func createPayload(xoFamilyVersion, gameName, space string) (sabre.ISabrePayload
 }
 
 // Build transaction
+<<<<<<< HEAD
 func createTransaction(sabrePayloadBuilder sabre.ISabrePayloadBuilder,
 	signer *signing.Signer) (*transaction_pb2.Transaction, error) {
+=======
+func createTransaction(sabrePayloadBuilder sabre.ISabrePayloadBuilder) (*transaction_pb2.Transaction, error) {
+>>>>>>> 86c23d9... Update test client to meet  sdk requirements
 	txnBuilder, err := t.NewTransactionBuilder()
 	if err != nil {
 		return nil, err
@@ -63,14 +90,24 @@ func createTransaction(sabrePayloadBuilder sabre.ISabrePayloadBuilder,
 		sabre.WithPayloadBuilder(sabrePayloadBuilder),
 		sabre.WithTransactionBuilder(txnBuilder),
 	)
+	if err != nil {
+		return nil, err
+	}
 
 	return sabreTxnBuilder.Build(signer)
 }
 
+<<<<<<< HEAD
 func createBatch(txns []*transaction_pb2.Transaction, signer *signing.Signer) ([]byte, error) {
+=======
+func createBatch(txns []*transaction_pb2.Transaction) ([]byte, error) {
+>>>>>>> 86c23d9... Update test client to meet  sdk requirements
 	batchBuilder, err := transactions.NewBatchBuilder(
 		transactions.WithTransactions(txns),
 	)
+	if err != nil {
+		return nil, err
+	}
 	return batchBuilder.Build(signer)
 }
 
